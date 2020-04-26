c005a19cbc327b261ba5629af774f4f8f392de5f
Mel <54912463+mel-f-dev@users.noreply.github.com>
2020-04-27 00:30:45 +0900

    circle image view

--- app/src/main/java/com/e/martineetalk/SignupActivity.kt
+++ app/src/main/java/com/e/martineetalk/SignupActivity.kt
@@ -12,6 +12,7 @@ import android.util.Log
 import android.widget.Toast
 import com.google.android.material.tabs.TabLayout
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_signup.*
@@ -64,9 +65,10 @@ class SignupActivity : AppCompatActivity() {
 
             selectedPhotoUri = data.data
             val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, selectedPhotoUri)
-            val bitmapDrawable = BitmapDrawable(bitmap)
-
-            signup_selectphoto_btn.setBackgroundDrawable(bitmapDrawable)
+            signup_selectphoto_imgview.setImageBitmap(bitmap)
+            signup_selectphoto_btn.alpha = 0f
+//            val bitmapDrawable = BitmapDrawable(bitmap)
+//            signup_selectphoto_btn.setBackgroundDrawable(bitmapDrawable)
 
         }
     }
@@ -109,6 +111,29 @@ class SignupActivity : AppCompatActivity() {
         ref.putFile(selectedPhotoUri!!)
             .addOnSuccessListener {
                 Log.d("SignupActivity", "Successfully uploaded image: ${it.metadata?.path}")
+
+                ref.downloadUrl.addOnSuccessListener {
+                    it.toString()
+                    Log.d("SignupActivity", "File Location: $it")
+
+                    saveUserToFirebaseDatabase(it.toString())
+                }
+            }
+            .addOnFailureListener{
+                //do some logging here
+            }
+    }
+
+    private fun saveUserToFirebaseDatabase(profileImageUri: String) {
+        val uid = auth.uid?: ""
+        val ref = FirebaseDatabase.getInstance().getReference("/users/$uid")
+        val user = User(uid, signup_edittext_name.text.toString(), profileImageUri)
+
+        ref.setValue(user)
+            .addOnSuccessListener {
+                Log.d("SignupActivity", "Finally we saved the user to Firebase Database")
             }
     }
 }
+
+class User(val uid: String, val username: String, val profileImageUri: String)
